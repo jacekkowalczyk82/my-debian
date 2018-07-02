85d36b1392da3eb2f87cda75e7960ee0d025e3ed
Jacek Kowalczyk <jacekkowalczyk82@gmail.com>
2018-07-02 09:22:13 +0200

    kali configs version 2018-07-02, fixed build scrip

--- build-i3wm.sh
+++ build-i3wm.sh
@@ -6,7 +6,7 @@ cp -v /opt/my-debian/i3/etc/skel/.i3wm_conkyrc /home/kowalczy/live-build-config/
 cp -v -r /opt/my-debian/i3/etc/skel/.config /home/kowalczy/live-build-config/kali-config/common/includes.chroot/etc/skel/
 cp -v -r /opt/my-debian/i3/etc/skel/.config /home/kowalczy/live-build-config/kali-config/common/includes.chroot/root/
 
-cp -v /opt/my-debian/variant-i3wm/package-lists/kali.list.chroot home/kowalczy/live-build-config/kali-config/variant-i3wm/package-lists/kali.list.chroot
+cp -v /opt/my-debian/variant-i3wm/package-lists/kali.list.chroot /home/kowalczy/live-build-config/kali-config/variant-i3wm/package-lists/kali.list.chroot
 
 cd /home/kowalczy/live-build-config/
 date
--- build-openbox.sh
+++ build-openbox.sh
@@ -5,7 +5,7 @@ cp -v /opt/my-debian/openbox/etc/skel/.openbox_conkyrc /home/kowalczy/live-build
 cp -v -r /opt/my-debian/openbox/etc/skel/.config /home/kowalczy/live-build-config/kali-config/common/includes.chroot/etc/skel/
 cp -v -r /opt/my-debian/openbox/etc/skel/.config /home/kowalczy/live-build-config/kali-config/common/includes.chroot/root/
 
-cp -v /opt/my-debian/variant-openbox/package-lists/kali.list.chroot home/kowalczy/live-build-config/kali-config/variant-openbox/package-lists/kali.list.chroot
+cp -v /opt/my-debian/variant-openbox/package-lists/kali.list.chroot /home/kowalczy/live-build-config/kali-config/variant-openbox/package-lists/kali.list.chroot
 
 cd /home/kowalczy/live-build-config/
 date
