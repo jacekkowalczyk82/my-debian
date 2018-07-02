e8fa3784535fda53f4e10d388a5e2cbec47da88c
Jacek Kowalczyk <jacekkowalczyk82@gmail.com>
2018-07-02 09:16:18 +0200

    kali configs version 2018-07-02

--- build-i3wm.sh
+++ build-i3wm.sh
@@ -1,9 +1,16 @@
 #/usr/bin/env bash 
-cp -v /opt/my-debian/i3/etc/skel/.i3wm_conkyrc /home/kowalczy/live-build-config/kali-config/common/includes.chroot/etc/skel/.conkyrc
-cp -v /opt/my-debian/i3/etc/skel/.i3wm_conkyrc /home/kowalczy/live-build-config/kali-config/common/includes.chroot/root/.conkyrc
 
+cp -v /opt/my-debian/i3/etc/skel/.i3wm_conkyrc /home/kowalczy/live-build-config/kali-config/common/includes.chroot/etc/skel/.i3wm_conkyrc
+cp -v /opt/my-debian/i3/etc/skel/.i3wm_conkyrc /home/kowalczy/live-build-config/kali-config/common/includes.chroot/root/.i3wm_conkyrc
+
+cp -v -r /opt/my-debian/i3/etc/skel/.config /home/kowalczy/live-build-config/kali-config/common/includes.chroot/etc/skel/
+cp -v -r /opt/my-debian/i3/etc/skel/.config /home/kowalczy/live-build-config/kali-config/common/includes.chroot/root/
+
+cp -v /opt/my-debian/variant-i3wm/package-lists/kali.list.chroot home/kowalczy/live-build-config/kali-config/variant-i3wm/package-lists/kali.list.chroot
+
+cd /home/kowalczy/live-build-config/
+date
 ./build.sh --distribution kali-rolling --variant i3wm --verbose
-
-
+date
 
 
--- build-openbox.sh
+++ build-openbox.sh
@@ -1,9 +1,16 @@
 #/usr/bin/env bash 
-cp -v /opt/my-debian/openbox/etc/skel/.openbox_conkyrc /home/kowalczy/live-build-config/kali-config/common/includes.chroot/etc/skel/.conkyrc
-cp -v /opt/my-debian/openbox/etc/skel/.openbox_conkyrc /home/kowalczy/live-build-config/kali-config/common/includes.chroot/root/.conkyrc
+cp -v /opt/my-debian/openbox/etc/skel/.openbox_conkyrc /home/kowalczy/live-build-config/kali-config/common/includes.chroot/etc/skel/.openbox_conkyrc
+cp -v /opt/my-debian/openbox/etc/skel/.openbox_conkyrc /home/kowalczy/live-build-config/kali-config/common/includes.chroot/root/.openbox_conkyrc
 
+cp -v -r /opt/my-debian/openbox/etc/skel/.config /home/kowalczy/live-build-config/kali-config/common/includes.chroot/etc/skel/
+cp -v -r /opt/my-debian/openbox/etc/skel/.config /home/kowalczy/live-build-config/kali-config/common/includes.chroot/root/
+
+cp -v /opt/my-debian/variant-openbox/package-lists/kali.list.chroot home/kowalczy/live-build-config/kali-config/variant-openbox/package-lists/kali.list.chroot
+
+cd /home/kowalczy/live-build-config/
+date
 ./build.sh --distribution kali-rolling --variant openbox --verbose
-
+date
 
 
 
