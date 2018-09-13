f38c7790ca6606171f45080b5143e0f28d6ae4da
Jacek Kowalczyk <jacekkowalczyk82@gmail.com>
2018-09-13 09:41:55 +0200

    adding customization for mate edition with openbox and i3wm

---
+++ build-mate_openbox.sh
@@ -0,0 +1,43 @@
+#/usr/bin/env bash 
+
+mkdir -p /home/kowalczy/live-build-config/kali-config/variant-mate_openbox/package-lists/
+mkdir -p /home/kowalczy/live-build-config/kali-config/common/includes.chroot/etc/skel/.config/
+mkdir -p /home/kowalczy/live-build-config/kali-config/common/includes.chroot/root/.config/
+mkdir -p /home/kowalczy/live-build-config/kali-config/common/includes.chroot/opt/firefox/
+mkdir -p /home/kowalczy/live-build-config/kali-config/common/includes.chroot/opt/toxic/
+mkdir -p /home/kowalczy/live-build-config/kali-config/common/includes.chroot/usr/bin/
+
+cp -v /opt/my-debian/mate_openbox/etc/skel/.openbox_conkyrc /home/kowalczy/live-build-config/kali-config/common/includes.chroot/etc/skel/.openbox_conkyrc
+cp -v /opt/my-debian/mate_openbox/etc/skel/.openbox_conkyrc /home/kowalczy/live-build-config/kali-config/common/includes.chroot/root/.openbox_conkyrc
+
+cp -v /opt/my-debian/i3/etc/skel/.i3wm_conkyrc /home/kowalczy/live-build-config/kali-config/common/includes.chroot/etc/skel/.i3wm_conkyrc
+cp -v /opt/my-debian/i3/etc/skel/.i3wm_conkyrc /home/kowalczy/live-build-config/kali-config/common/includes.chroot/root/.i3wm_conkyrc
+
+cp -v -r /opt/my-debian/mate_openbox/etc/skel/.config /home/kowalczy/live-build-config/kali-config/common/includes.chroot/etc/skel/
+cp -v -r /opt/my-debian/mate_openbox/etc/skel/.config /home/kowalczy/live-build-config/kali-config/common/includes.chroot/root/
+
+cp -v /opt/my-debian/variant-mate_openbox/package-lists/kali.list.chroot /home/kowalczy/live-build-config/kali-config/variant-mate_openbox/package-lists/kali.list.chroot
+
+#firefox
+cp -v -r /opt/firefox /home/kowalczy/live-build-config/kali-config/common/includes.chroot/opt/
+
+sleep 60
+
+cp -v /opt/my-debian/opt/firefox/firefox.sh /home/kowalczy/live-build-config/kali-config/common/includes.chroot/usr/bin/firefox.sh
+
+cp -v /opt/my-debian/opt/firefox/firefox-developer-edition.desktop /home/kowalczy/live-build-config/kali-config/common/includes.chroot/opt/firefox/firefox-developer-edition.desktop
+
+#toxic
+cp -v -r  /opt/toxic /home/kowalczy/live-build-config/kali-config/common/includes.chroot/opt/
+
+#i3lock 
+cp -v -r  /opt/my-debian/i3/kali-lock.sh /home/kowalczy/live-build-config/kali-config/common/includes.chroot/usr/bin/kali-lock.sh
+
+cd /home/kowalczy/live-build-config/
+date
+rm -rf config/
+./build.sh --distribution kali-rolling --variant openbox --verbose
+date
+
+
+
