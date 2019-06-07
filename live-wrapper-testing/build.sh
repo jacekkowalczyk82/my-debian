1657da16cec811d0f170b985d12331c2fbd5b20a
Jacek Kowalczyk <jacekkowalczyk82@gmail.com>
2019-06-07 14:40:04 +0200

    trying different tool for building rolling distro , updated

--- live-wrapper-testing/build.sh
+++ live-wrapper-testing/build.sh
@@ -3,9 +3,18 @@
 # sudo lwr -d testing
 # sudo lwr # for stretch 
 # sudo lwr -d buster 
+#/usr/share/live-wrapper/customise.sh 
+# --customise=`pwd`/customise.sh
 
 PACKAGES="dwm,suckless-tools,stterm,nitrogen,sudo,git,htop,nano,vim"
-PACKAGES_AND_RECOMMENDED="live-task-xfce,xorg,live-boot,live-config,live-config-systemd"
-lwr -d testing  -e ${PACKAGES} -t  ${PACKAGES_AND_RECOMMENDED} 
+PACKAGES_AND_RECOMMENDED="live-task-xfce,xorg,live-config-systemd"
+LOGFILE="debian-live-lwr-build-`date '+%Y-%m-%d_%H%M%S'`.log"
+
+MY_LWR_OPTIONS="--isolinux --grub --installer --di-daily --log-level=debug --log=${LOGFILE}"
+lwr -d testing -o debian-live-xfce-dwm.iso ${MY_LWR_OPTIONS} \
+ -e ${PACKAGES} \ 
+ -t  ${PACKAGES_AND_RECOMMENDED} 
+
+
 
 
