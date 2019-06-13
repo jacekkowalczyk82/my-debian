c91e3aba069ce3bba016f819e1e46ed1e996a6a8
Jacek Kowalczyk <jacekkowalczyk82@gmail.com>
2019-06-13 10:50:33 +0200

    kde dwm edition script added

---
+++ build-debian-buster-kde-dwm.sh
@@ -0,0 +1,23 @@
+#/bin/bash 
+
+cd live-build-buster-kde
+
+lb config --debian-installer live -d buster \
+ --debian-installer-distribution daily \
+ --iso-preparer "Jacek Kowalczyk jack82@bsdmail.com" 
+ 
+#add your packages to config/package-lists/live.list.chroot
+#add your customization files to config/includes.chroot/
+
+mkdir -p config/includes.chroot/opt/
+cp -rv suckless.org/dwm config/includes.chroot/opt/
+
+#when rebuilding run also clean
+#sudo lb clean --purge
+
+#build ISO
+lb build --debug --verbose 2>&1 |tee lb-build-buster-kde-dwm-`date '+%Y-%m-%d_%H%M%S'`.log
+
+
+
+
