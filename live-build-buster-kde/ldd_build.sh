ec19da7a9401e35c994fa4e040dd1a8936dff4dd
Jacek Kowalczyk <jacekkowalczyk82@gmail.com>
2019-12-18 07:38:02 +0000

    Update ldd_build.sh, fixed

--- live-build-buster-kde/ldd_build.sh
+++ live-build-buster-kde/ldd_build.sh
@@ -1,7 +1,5 @@
 #/bin/bash 
 
-
-
 lb config --debian-installer live -d buster \
  --grub-splash grub2.png \
  --debian-installer-distribution buster \
@@ -26,25 +24,24 @@ lb build --debug --verbose 2>&1 |tee debian-live-10-ldd-kde-`date '+%Y-%m-%d_%H%
 
 #`date '+%Y-%m-%d_%H%M%S'`
 GENERATED_NAME="debian-live-10-ldd-kde-amd64"
-RELEASE_NAME="debian-live-10-ldd-kde-`date '+%Y-%m-%d_%H%M%S'`-x64"
+RELEASE_NAME="debian-live-10-ldd-kde-`date '+%Y-%m-%d_%H%M%S'`-amd64"
 RELEASE_DATE=`date '+%Y%m%d'`
 
-if [[ -e "$GENERATED_NAME" ]]; then 
+if [[ -e "${GENERATED_NAME}.hybrid.iso" ]]; then 
 
-mv ${GENERATED_NAME}.hybrid.iso  ${RELEASE_NAME}.iso
-sha256sum ${RELEASE_NAME}.iso | tee -a ${RELEASE_NAME}.sha256sum.txt
+    mv ${GENERATED_NAME}.hybrid.iso  ${RELEASE_NAME}.iso
+    sha256sum ${RELEASE_NAME}.iso | tee -a ${RELEASE_NAME}.sha256sum.txt
 
-FILES_PATTERN="debian-live-10-ldd-kde"
+    FILES_PATTERN="debian-live-10-ldd-kde"
 
+    aws s3 --region us-east-2 cp ${FILES_PATTERN}-*.log  s3://jacekkowalczyk82.private.s3/my-debian-images/${RELEASE_DATE}/
+    aws s3 --region us-east-2 cp ${RELEASE_NAME}.iso s3://jacekkowalczyk82.private.s3/my-debian-images/${RELEASE_DATE}/
+    aws s3 --region us-east-2 cp ${RELEASE_NAME}.sha256sum.txt s3://jacekkowalczyk82.private.s3/my-debian-images/${RELEASE_DATE}/
 
-aws s3 --region us-east-2 cp ${FILES_PATTERN}-*.log  s3://jacekkowalczyk82.private.s3/my-debian-images/${RELEASE_DATE}/
-aws s3 --region us-east-2 cp ${RELEASE_NAME}.iso s3://jacekkowalczyk82.private.s3/my-debian-images/${RELEASE_DATE}/
-aws s3 --region us-east-2 cp ${RELEASE_NAME}.sha256sum.txt s3://jacekkowalczyk82.private.s3/my-debian-images/${RELEASE_DATE}/
-
-aws s3 --region us-east-2 cp ${GENERATED_NAME}.contents         s3://jacekkowalczyk82.private.s3/my-debian-images/${RELEASE_DATE}/
-aws s3 --region us-east-2 cp ${GENERATED_NAME}.files         s3://jacekkowalczyk82.private.s3/my-debian-images/${RELEASE_DATE}/
-aws s3 --region us-east-2 cp ${GENERATED_NAME}.hybrid.iso.zsync  s3://jacekkowalczyk82.private.s3/my-debian-images/${RELEASE_DATE}/
-aws s3 --region us-east-2 cp ${GENERATED_NAME}.packages      s3://jacekkowalczyk82.private.s3/my-debian-images/${RELEASE_DATE}/
+    aws s3 --region us-east-2 cp ${GENERATED_NAME}.contents         s3://jacekkowalczyk82.private.s3/my-debian-images/${RELEASE_DATE}/
+    aws s3 --region us-east-2 cp ${GENERATED_NAME}.files         s3://jacekkowalczyk82.private.s3/my-debian-images/${RELEASE_DATE}/
+    aws s3 --region us-east-2 cp ${GENERATED_NAME}.hybrid.iso.zsync  s3://jacekkowalczyk82.private.s3/my-debian-images/${RELEASE_DATE}/
+    aws s3 --region us-east-2 cp ${GENERATED_NAME}.packages      s3://jacekkowalczyk82.private.s3/my-debian-images/${RELEASE_DATE}/
 
 else 
 	echo "ERROR: failed to build ISO"
