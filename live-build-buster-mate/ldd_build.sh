8d5b2178025b5cc4e92fdd47bd24b15b4393f298
Jacek Kowalczyk <jacekkowalczyk82@gmail.com>
2020-02-18 09:27:55 +0000

    Update ldd_build.sh

--- live-build-buster-mate/ldd_build.sh
+++ live-build-buster-mate/ldd_build.sh
@@ -20,7 +20,7 @@ cp -rv ../bootloaders ./config/
 #sudo lb clean --purge
 
 #build ISO
-lb build --debug --verbose 2>&1 |tee debian-live-10-ldd-xfce-`date '+%Y-%m-%d_%H%M%S'`.log
+lb build --debug --verbose 2>&1 |tee debian-live-10-ldd-mate-`date '+%Y-%m-%d_%H%M%S'`.log
 
 #`date '+%Y-%m-%d_%H%M%S'`
 GENERATED_NAME="debian-live-10-ldd-mate-amd64"
