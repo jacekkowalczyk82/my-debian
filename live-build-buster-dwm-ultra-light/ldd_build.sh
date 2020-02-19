ad9cdca4e149b3668ecca002727653336d662dda
Jacek Kowalczyk <jacekkowalczyk82@gmail.com>
2020-02-19 12:17:03 +0100

    cleaned ldd_build scripts

--- live-build-buster-dwm-ultra-light/ldd_build.sh
+++ live-build-buster-dwm-ultra-light/ldd_build.sh
@@ -1,10 +1,10 @@
 #/bin/bash 
-
+LDD_EDITION="ultra"
 lb config --debian-installer live -d buster \
  --grub-splash grub2.png \
  --debian-installer-distribution buster \
- --iso-application debian-live-10-ldd-ultra \
- --iso-volume debian-live-10-ldd-ultra \
+ --iso-application debian-live-10-ldd-${LDD_EDITION} \
+ --iso-volume debian-live-10-ldd-${LDD_EDITION} \
  --iso-publisher "Jacek Kowalczyk http://jacekkowalczyk82.github.io" \
  --iso-preparer "Jacek Kowalczyk http://jacekkowalczyk82.github.io" 
  
@@ -22,11 +22,11 @@ cp -rv ../bootloaders ./config/
 #build ISO
 lb clean
 
-lb build --debug --verbose 2>&1 |tee debian-live-10-ldd-ultra-`date '+%Y-%m-%d_%H%M%S'`.log
+lb build --debug --verbose 2>&1 |tee debian-live-10-ldd-${LDD_EDITION}-`date '+%Y-%m-%d_%H%M%S'`.log
 
 #`date '+%Y-%m-%d_%H%M%S'`
-GENERATED_NAME="debian-live-10-ldd-ultra-amd64"
-RELEASE_NAME="debian-live-10-ldd-ultra-`date '+%Y-%m-%d_%H%M%S'`-amd64"
+GENERATED_NAME="debian-live-10-ldd-${LDD_EDITION}-amd64"
+RELEASE_NAME="debian-live-10-ldd-${LDD_EDITION}-`date '+%Y-%m-%d_%H%M%S'`-amd64"
 RELEASE_DATE=`date '+%Y%m%d'`
 
 if [ -e "${GENERATED_NAME}.hybrid.iso" ]; then 
@@ -34,7 +34,7 @@ if [ -e "${GENERATED_NAME}.hybrid.iso" ]; then
     mv ${GENERATED_NAME}.hybrid.iso  ${RELEASE_NAME}.iso
     sha256sum ${RELEASE_NAME}.iso | tee -a ${RELEASE_NAME}.sha256sum.txt
 
-    FILES_PATTERN="debian-live-10-ldd-ultra"
+    FILES_PATTERN="debian-live-10-ldd-${LDD_EDITION}"
 
     aws s3 --region us-east-2 cp ${FILES_PATTERN}-*.log  s3://jacekkowalczyk82.private.s3/my-debian-images/${RELEASE_DATE}/
     aws s3 --region us-east-2 cp ${RELEASE_NAME}.iso s3://jacekkowalczyk82.private.s3/my-debian-images/${RELEASE_DATE}/
--- live-build-buster/ldd_build.sh
+++ live-build-buster/ldd_build.sh
@@ -1,10 +1,10 @@
 #/bin/bash 
-
+LDD_EDITION="xfce"
 lb config --debian-installer live -d buster \
  --grub-splash grub2.png \
  --debian-installer-distribution buster \
- --iso-application debian-live-10-ldd-xfce \
- --iso-volume debian-live-10-ldd-xfce \
+ --iso-application debian-live-10-ldd-${LDD_EDITION} \
+ --iso-volume debian-live-10-ldd-${LDD_EDITION} \
  --iso-publisher "Jacek Kowalczyk http://jacekkowalczyk82.github.io" \
  --iso-preparer "Jacek Kowalczyk http://jacekkowalczyk82.github.io" 
  
@@ -22,19 +22,19 @@ cp -rv ../bootloaders ./config/
 #build ISO
 lb clean
 
-lb build --debug --verbose 2>&1 |tee debian-live-10-ldd-xfce-`date '+%Y-%m-%d_%H%M%S'`.log
+lb build --debug --verbose 2>&1 |tee debian-live-10-ldd-${LDD_EDITION}-`date '+%Y-%m-%d_%H%M%S'`.log
 
 #`date '+%Y-%m-%d_%H%M%S'`
-GENERATED_NAME="debian-live-10-ldd-xfce-amd64"
-RELEASE_NAME="debian-live-10-ldd-xfce-`date '+%Y-%m-%d_%H%M%S'`-amd64"
+GENERATED_NAME="debian-live-10-ldd-${LDD_EDITION}-amd64"
+RELEASE_NAME="debian-live-10-ldd-${LDD_EDITION}-`date '+%Y-%m-%d_%H%M%S'`-amd64"
 RELEASE_DATE=`date '+%Y%m%d'`
 
-if [[ -e "${GENERATED_NAME}.hybrid.iso" ]]; then 
+if [ -e "${GENERATED_NAME}.hybrid.iso" ]; then 
 
     mv ${GENERATED_NAME}.hybrid.iso  ${RELEASE_NAME}.iso
     sha256sum ${RELEASE_NAME}.iso | tee -a ${RELEASE_NAME}.sha256sum.txt
 
-    FILES_PATTERN="debian-live-10-ldd-xfce"
+    FILES_PATTERN="debian-live-10-ldd-${LDD_EDITION}"
 
     aws s3 --region us-east-2 cp ${FILES_PATTERN}-*.log  s3://jacekkowalczyk82.private.s3/my-debian-images/${RELEASE_DATE}/
     aws s3 --region us-east-2 cp ${RELEASE_NAME}.iso s3://jacekkowalczyk82.private.s3/my-debian-images/${RELEASE_DATE}/
